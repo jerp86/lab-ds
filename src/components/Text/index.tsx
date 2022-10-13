0c399359a6f307215671bef8616e3f892ad13328
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-10-12 22:28:39 -0300

    add Slot

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,17 +1,21 @@
 import { ReactNode } from "react";
 import { clsx } from "clsx";
+import { Slot } from "@radix-ui/react-slot";
 
 export interface TextProps {
-  size?: "sm" | "md" | "lg";
   children: ReactNode;
+  asChild?: boolean;
+  size?: "sm" | "md" | "lg";
 }
 
-export const Text = ({ children, size = "md" }: TextProps) => {
+export const Text = ({ children, asChild, size = "md" }: TextProps) => {
   const className = clsx("text-gray-100 font-sans", {
     "text-xs": size === "sm",
     "text-sm": size === "md",
     "text-md": size === "lg",
   });
 
-  return <span className={className}>{children}</span>;
+  const Comp = asChild ? Slot : "span";
+
+  return <Comp className={className}>{children}</Comp>;
 };
