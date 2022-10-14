0cfe1936b1d760187c3b9399677370cf4b85e727
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-10-14 10:38:20 -0300

    create SignIn page

---
+++ src/pages/SignIn/SignIn.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { SignIn } from ".";
+
+export default {
+  title: "Pages/Sign in",
+  component: SignIn,
+  args: {},
+  argsTypes: {},
+} as Meta;
+
+export const Default: StoryObj = {};
