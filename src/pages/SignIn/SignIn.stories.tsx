3958db8902c26ebe97888c2afb5193197dd12e65
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-10-15 18:50:18 -0300

    add test

--- src/pages/SignIn/SignIn.stories.tsx
+++ src/pages/SignIn/SignIn.stories.tsx
@@ -1,4 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { userEvent, waitFor, within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
 
 import { SignIn } from ".";
 
@@ -9,4 +11,20 @@ export default {
   argsTypes: {},
 } as Meta;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText("Digite seu e-mail"),
+      "jerp.dev@gmail.com"
+    );
+    userEvent.type(canvas.getByPlaceholderText("******"), "123456");
+
+    userEvent.click(canvas.getByRole("button"));
+
+    waitFor(() =>
+      expect(canvas.getByText("Login realizado!")).toBeInTheDocument()
+    );
+  },
+};
