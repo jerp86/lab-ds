91f42af34f7ea7147ee9669ab42a3dca47a0efbd
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-10-15 22:46:22 -0300

    add parameters

--- src/pages/SignIn/SignIn.stories.tsx
+++ src/pages/SignIn/SignIn.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { userEvent, waitFor, within } from "@storybook/testing-library";
 import { expect } from "@storybook/jest";
+import { rest } from "msw";
 
 import { SignIn } from ".";
 
@@ -9,6 +10,15 @@ export default {
   component: SignIn,
   args: {},
   argsTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post("/sessions", (req, res, ctx) =>
+          res(ctx.json({ message: "Login realizado!" }))
+        ),
+      ],
+    },
+  },
 } as Meta;
 
 export const Default: StoryObj = {
